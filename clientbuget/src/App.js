f733d7ee029984fabb923f91943c09d849581cf9
unknown <vladmoisei@yahoo.com>
2021-03-25 23:13:51 +0200

    Add bootstrap and reactstrap, plus layout

--- clientbuget/src/App.js
+++ clientbuget/src/App.js
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Home } from "./Pages/Home";
+import { Counter } from "./Components/Counter";
 import { FetchData } from "./Pages/FetchData";
+import { Layout } from "./Components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <Layout>
         <Routes>
-          <Route path="" element={<Home />} />
-          <Route path="fetchdata" element={<FetchData />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/fetchdata" element={<FetchData />} />
+          <Route path="/counter" element={<Counter />} />
         </Routes>
-      </div>
+      </Layout>
     </BrowserRouter>
   );
 }
